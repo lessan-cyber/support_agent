42869f267d5e4816cb0296de375724bd1f62c242
lessan-cyber <azizmetchonou434@gmail.com>
2025-11-27 17:24:31 +0000

    chore: update app settings

--- app/settings.py
+++ app/settings.py
@@ -4,6 +4,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL: PostgresDsn = Field(..., alias="DATABASE_URL")
+    DEBUG: bool = Field(..., alias="DEBUG")
+    TEST_URL: PostgresDsn = Field(..., alias="TEST_URL")
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 
