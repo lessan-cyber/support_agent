653899618e1a160fc8593ba966f2187d90fd423d
lessan-cyber <azizmetchonou434@gmail.com>
2025-12-01 15:23:58 +0000

    feat(core) adding service key to the settings

--- app/settings.py
+++ app/settings.py
@@ -10,9 +10,10 @@ class Settings(BaseSettings):
     )  # this is a non root user just for testing RLS policies
     REDIS_URL: RedisDsn = Field(..., alias="REDIS_URL")
     SUPABASE_URL: str = Field(..., alias="SUPABASE_URL")
-    SUPABASE_KEY: str = Field(..., alias="SUPABASE_KEY")
+    SUPABASE_ANON_KEY: str = Field(..., alias="SUPABASE_ANON_KEY")
     KNOWLEDGE_BASE_BUCKET: str = Field(..., alias="KNOWLEDGE_BASE_BUCKET")
     SUPABASE_JWT_SECRET: str = Field(..., alias="SUPABASE_JWT_SECRET")
+    SUPABASE_SERVICE_KEY: str = Field(..., alias="SUPABASE_SERVICE_KEY")
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 
