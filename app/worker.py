9b4e66e8ff19326b52b768db79be97d6ce12e49c
lessan-cyber <azizmetchonou434@gmail.com>
2025-12-01 15:21:00 +0000

    feat(core) adding configuration for celery worker

--- app/worker.py
+++ app/worker.py
@@ -8,11 +8,10 @@ celery_app = Celery(
     "worker",
     broker=str(settings.REDIS_URL),
     backend=str(settings.REDIS_URL),
-    include=["app.services.ingestion"],  # Points to the module where tasks are defined
+    include=["app.services.ingestion"],
 )
 
-# Optional: Configure Celery further if needed
-# pyrefly: ignore [missing-attribute]
+
 celery_app.conf.update(
     task_track_started=True,
 )
